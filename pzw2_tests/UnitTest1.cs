5dc8970c741b8dff92c30f0e90be868e8e8dbb99
barisin <mbaris00@fesb.hr>
2015-03-29 18:36:11 +0200

    First Commit

---
+++ pzw2_tests/UnitTest1.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using testing_pzw2;
+
+namespace pzw2_tests
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void DefaultConstructorCheck()
+        {
+            Date a = new Date();
+            Assert.AreEqual(0,a.day,"Wrong day in default contructor!");            
+        }
+
+        [TestMethod]
+        public void SetConstructorCheck()
+        {
+            Date a = new Date(29,3,2015);
+            Assert.AreEqual(3, a.month, "Wrong month in set constructor!");
+        }
+
+        [TestMethod]
+        public void isLeapYearFalseCheck()
+        {
+            Date a = new Date(22, 3, 2015);
+            Boolean flag = a.isLeapYear();
+            Assert.AreEqual(false, flag, "Error in leap year check! Should be false!");
+        }
+
+        [TestMethod]
+        public void isLeapYearTrueCheck()
+        {
+            Date a = new Date(22, 3, 2016);
+            Boolean flag = a.isLeapYear();
+            Assert.AreEqual(true, flag, "Error in leap year check! Should be true!");
+        }
+
+        [TestMethod]
+        public void isLeapYearTrue400sCheck()
+        {
+            Date a = new Date(22, 3, 2000);
+            Boolean flag = a.isLeapYear();
+            Assert.AreEqual(true, flag, "Error in leap year check! Should be true!");
+        }
+
+        [TestMethod]
+        public void isLeapYearTrue100sCheck()
+        {
+            Date a = new Date(22, 3, 2100);
+            Boolean flag = a.isLeapYear();
+            Assert.AreEqual(false, flag, "Error in leap year check! Should be false!");
+        }
+
+
+    }
+}
---
+++ testing_pzw2/Date.cs
@@ -0,0 +1,152 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace testing_pzw2
+{
+    public class Date
+    {
+        public int day, month, year;
+
+        public Date()
+        {
+            day = 0;
+            month = 0;
+            year = 0;
+        }
+
+        public Date(int d, int m, int y)
+        {
+            day = d;
+            month = m;
+            year = y;
+        }
+
+        public string getMonthName()
+        {
+            string name;
+            switch(month)
+            {
+                case 1:
+                    {
+                        name = "Siječanj";
+                        break;
+                    }
+                case 2:
+                    {
+                        name = "Valjača";
+                        break;
+                    }
+                case 3:
+                    {
+                        name = "Ožujak";
+                        break;
+                    }
+                case 4:
+                    {
+                        name = "Travanj";
+                        break;
+                    }
+                case 5:
+                    {
+                        name = "Svibanj";
+                        break;
+                    }
+                case 6:
+                    {
+                        name = "Lipanj";
+                        break;
+                    }
+                case 7:
+                    {
+                        name = "Srpanj";
+                        break;
+                    }
+                case 8:
+                    {
+                        name = "Kolokvoz";
+                        break;
+                    }
+                case 9:
+                    {
+                        name = "Rujan";
+                        break;
+                    }
+                case 10:
+                    {
+                        name = "Listopad";
+                        break;
+                    }
+                case 11:
+                    {
+                        name = "Studeni";
+                        break;
+                    }
+                case 12:
+                    {
+                        name = "Prosinac";
+                        break;
+                    }
+                default:
+                    {
+                        name = "";
+                        break;
+                    }
+            }
+
+            return name;
+        }
+
+        public bool isLeapYear()
+        {
+            if ((year % 4 == 0 && year % 100 != 0) || year % 400 == 0)
+                return true;
+            else
+                return false;
+        }
+
+        public int getNumberOfRemaingDaysInMonth()
+        {
+            int broj;
+            switch(month)
+            {
+                case 1:
+                case 3:
+                case 5:
+                case 7:
+                case 8:
+                case 10:
+                case 12:
+                    {
+                        broj = 31 - day;
+                        break;
+                    }
+                case 4:
+                case 6:
+                case 9:
+                case 11:
+                    {
+                        broj = 30 - day;
+                        break;
+                    }
+                case 2:
+                    {
+                        if (isLeapYear())
+                            broj = 28 - day;
+                        else
+                            broj = 29 - day;
+                        break;
+                    }
+                default:
+                    {
+                        broj = -1;
+                        break;
+                    }
+            }
+
+            return broj;
+        }
+    }
+}
